5dcf354120abb7959364e83907e40a448750ee67
Junhyung Park <junhyung1821@gmail.com>
2020-12-11 13:38:01 -0600

    images, videos, tiles, save JSON

--- talkmeup_tech_quiz_2/src/App.js
+++ talkmeup_tech_quiz_2/src/App.js
@@ -1,38 +1,66 @@
 import './App.css';
 import React from 'react';
+import Board from './board/board';
+import ImageSearch from './imageSearch/imageSearch'
+import BoardVideo from './board/boardVideo';
+
+const API_KEY = "19435337-59a77b980b37e8c813d079adb"
+
 
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    images: [],
+    videos: [],
+    isSubmitted: false
+  };
 
-    this.state = {value:''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange (event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('The item ' + this.state.value + ' is searched.');
+  handleGetRequest = async (event) => {
     event.preventDefault();
+
+    const searchValue = event.target.elements.searchValue.value;
+
+    const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${searchValue}&image_type=photo&per_page=7`;
+    const URL_video = `https://pixabay.com/api/videos/?key=${API_KEY}&q=${searchValue}&per_page=3`;
+    
+    const request = await fetch(URL);
+    const response = await request.json();
+
+    const request_video = await fetch(URL_video);
+    const response_video = await request_video.json();
+
+    this.setState({ images : response.hits });
+    this.setState({ videos : response_video.hits});
+
+    this.setState({ isSubmitted : true});
   }
-
-
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Search: 
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit"/>
-      </form>
-    );
+      <div>
+        <ImageSearch handleGetRequest={this.handleGetRequest}/>
+        {this.state.isSubmitted &&
+        <div className="table">
+          Click the desired tile to save the image or video on the tile as JSON:
+          <div>
+            <Board images={this.state.images[0]}/>
+            <Board images={this.state.images[1]}/>
+            <Board images={this.state.images[2]}/>
+          </div>
+          <div>
+            <Board images={this.state.images[3]}/>
+            <Board images={this.state.images[4]}/>
+            <Board images={this.state.images[5]}/>
+          </div>
+          <div>
+            <Board images={this.state.images[6]}/>
+            <BoardVideo videos={this.state.videos[0]}/>
+            <BoardVideo videos={this.state.videos[1]}/>
+          </div>
+        </div>
+        }
+      </div>
+    )
   }
 }
 
